753099b03e092d54748420351d5f3029c5ab5299
Serhii Shylo <shilkas2@gmail.com>
2019-03-05 21:03:00 +0200

    Урок 4. JavaScript

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert('Hello, world');
+// console.log('Hello, console');
+
+// var messageText;
+// var count = 2;
+//
+// var message1 = 'fff', label, message3 = 'ddd';
+//
+// messageText = 'Текст сообщения';
+//
+// alert(messageText);
+
+var num1, num2, result;
+
+num1 = +prompt('Введите 1-ое число', '0');
+num2 = +prompt('Введите 2-ое число', '0');
+
+result = num1 + num2;
+
+alert('Результат сложения' + result);
