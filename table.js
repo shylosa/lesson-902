c1067a0193539efc513dd6976b2cd2ac0bcb6abd
Serhii Shylo <shilkas2@gmail.com>
2019-03-19 21:21:46 +0200

    Занятие 7

--- table.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let table = document.body.children[0];
-
-for (let i = 0; i < table.rows.length; i++) {
-  let row = table.rows[i];
-  row.cells[i].style.backgroundColor = 'red';
-}
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Hello, world!\n";
+
+$a = ['d', 'e', 'k', 'la'];
+$result = '';
+foreach ($a as $item) {
+    $result .= $item;
+}
+
+echo $result . '<br>';
